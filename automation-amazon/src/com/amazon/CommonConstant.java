bbf8fe479c9c78cb4c66d92bab8c2495ebf06e99
karishmakhan12 <karishmakhanbangalore@outlook.com>
2021-10-09 18:21:28 +0530

    amazon automation code added

---
+++ automation-amazon/src/com/amazon/CommonConstant.java
@@ -0,0 +1,7 @@
+package com.amazon;
+
+public class CommonConstant {
+
+	public static final String WEB_CHROME_DRIVER= "webdriver.chrome.driver";
+	public static final String WEB_CHROME_DRIVER_PATH = "C:\\Program Files\\selenium-jars-drivers\\drivers\\chromeVersion94\\chromedriver.exe";
+}
--- automation-gmail/src/com/gmail/CommonConstant.java
+++ automation-gmail/src/com/gmail/CommonConstant.java
@@ -3,5 +3,5 @@ package com.gmail;
 public class CommonConstant {
 
 	public static final String WEB_CHROME_DRIVER= "webdriver.chrome.driver";
-	public static final String WEB_CHROME_DRIVER_PATH = "C:\\selenium-jar-drivers\\drivers\\chrome\\chromedriver.exe";
+	public static final String WEB_CHROME_DRIVER_PATH = "C:\\\\Program Files\\\\selenium-jars-drivers\\\\drivers\\\\chrome\\\\version-94\\\\chromedriver.exe";
 }
